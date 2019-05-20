fb9b0da3b5e1cbae7be8298dc678e4288b168e72
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-20 22:31:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(array)
+  array.sort
+end
+  
+def sort_array_desc(array)
+  array.sort.reverse
+end
 
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
